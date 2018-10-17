9b6d606a2eafe874265abfb01b39534a442f3f94
CarlGauss <staff@Staffs-MacBook-Pro.local>
2018-10-17 16:46:51 +0700

    adjust secret key

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,5 +1,5 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyBHhIb3xEcgw2mhC_pIUkJb-PmkK-Lm0T8",
-  authDomain: "todo-14682.firebaseapp.com",
-  databaseURL: "https://todo-14682.firebaseio.com",
+  apiKey: "*****",
+  authDomain: "******.firebaseapp.com",
+  databaseURL: "https://*****.firebaseio.com",
 };
